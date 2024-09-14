ed09205a89b00ed0debc1ced7c13301490864724
theGobindSingh <thesinghgobind@gmail.com>
2024-09-15 01:07:04 +0530

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { Global } from '@emotion/react';
+import globalStyles from '@styles/global';
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </>
+  );
+}
